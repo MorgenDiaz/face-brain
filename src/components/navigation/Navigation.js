ee1c7a3283b37ddd5df886688d9a907714252f22
Morgen Diaz <morgen@webslinger.works>
2022-05-27 13:40:20 -1000

    Setup navigation component.

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./Navigation.css";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <p>Sign Out</p>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
